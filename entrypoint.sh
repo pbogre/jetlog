bd9b2f41a21e6bbc8201320f1b39b4f28fd022eb
Pietro Bonaldo <pietro.bogre@gmail.com>
2024-08-02 11:17:14 +0200

    Set data ownership when root but user specified (#35)

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,17 @@ start_command="python -m \
                --host 0.0.0.0 \
                --port ${JETLOG_PORT}"
 
+function run_as_root()
+{
+    echo "Could not set data folder ownership, running Jetlog as root..."
+    ${start_command}
+    exit 0
+}
+
 if [ $(id -u) -eq 0 ] && [ $(id -g) -eq 0 ];
 then
-    #echo "Container running as root, setting permissions to ${PUID}:${PGID}..."
-    echo "Container running as root..."
-    #chown -R ${PUID}:${PGID} ${DATA_PATH}
+    echo "Container running as root, setting permissions to ${PUID}:${PGID}..."
+    chown -R ${PUID}:${PGID} ${DATA_PATH} || run_as_root
     gosu ${PUID}:${PGID} ${start_command}
 else
     echo "Container running as $(id -u):$(id -g)..."
