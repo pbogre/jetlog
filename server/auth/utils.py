b47ecd15dbcea538a6c59d6690d39582ebc1e1c7
Pietro Bonaldo <pietro.bogre@gmail.com>
2024-12-04 10:56:15 +0100

    Use argon2 hashing instead of bcrypt (#60)

--- server/auth/utils.py
+++ server/auth/utils.py
@@ -1,19 +1,16 @@
 from server.models import User
 
 from fastapi.security import OAuth2PasswordBearer
-import bcrypt
+from argon2 import PasswordHasher
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/token", auto_error=False)
+_ph = PasswordHasher()
 
 def verify_password(password: str, password_hash: str) -> bool:
-    password_bytes = password.encode('utf-8')
-    password_hash_bytes = password_hash.encode('utf-8')
-    return bcrypt.checkpw(password_bytes, password_hash_bytes)
+    return _ph.verify(password_hash, password)
 
 def hash_password(password: str) -> str:
-    pwd_bytes = password.encode('utf-8')
-    salt = bcrypt.gensalt()
-    password_hash = bcrypt.hashpw(pwd_bytes, salt).decode('utf-8')
+    password_hash = _ph.hash(password)
     return password_hash
 
 def get_user(username: str) -> User|None:
