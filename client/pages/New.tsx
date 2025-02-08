79813f21b182d3f02329121711e01b2af9dde8cf
Pietro Bonaldo <pietro.bogre@gmail.com>
2025-02-08 17:43:11 +0100

    UI Spacing improvements

--- client/pages/New.tsx
+++ client/pages/New.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { Heading, Label, Button, Input, Select, TextArea } from '../components/Elements'
+import { Heading, Label, Button, Input, Select, TextArea } from '../components/Elements';
 import AirportInput from '../components/AirportInput';
 
 import API from '../api';
@@ -20,106 +20,142 @@ export default function New() {
         }
 
         API.post("/flights", flightData)
-        .then(() => navigate("/"));
-    }
+            .then(() => navigate("/"));
+    };
+
     return (
-    <>
-        <Heading text="New Flight" />
+        <>
+            <Heading text="New Flight" />
 
-        <form onSubmit={postFlight}>
-            <div className="flex flex-wrap">
+            <form onSubmit={postFlight}>
+                <div className="flex flex-wrap">
+                    <div className="container">
+                        <Label text="Origin" required />
+                        <AirportInput name="origin" />
+                        <br />
+                        <Label text="Destination" required />
+                        <AirportInput name="destination" />
+                        <br />
+                        <Label text="Date" required />
+                        <Input
+                            type="date"
+                            name="date"
+                            defaultValue={(new Date()).toISOString().substring(0, 10)}
+                            required
+                        />
+                    </div>
 
-                <div className="container">
-                    <Label text="Origin" required />
-                    <AirportInput name="origin" />
-                    <br />
-                    <Label text="Destination" required />
-                    <AirportInput name="destination" />
-                    <br />
-                    <Label text="Date" required />
-                    <Input type="date"
-                           name="date"
-                           defaultValue={(new Date()).toISOString().substring(0, 10)} // today's date 
-                           required />
+                    <div className="container">
+                        <Label text="Departure Time" />
+                        <Input
+                            type="time"
+                            name="departureTime"
+                        />
+                        <br />
+                        <Label text="Arrival Time" />
+                        <Input
+                            type="time"
+                            name="arrivalTime"
+                        />
+                        <br />
+                        <Label text="Arrival Date" />
+                        <Input
+                            type="date"
+                            name="arrivalDate"
+                        />
+                    </div>
+
+                    <div className="container">
+                        <div className="flex justify-between flex-wrap gap-4 items-start">
+                            <div className="flex flex-col">
+                                <Label text="Seat Type" />
+                                <Select
+                                    name="seat"
+                                    options={[
+                                        { text: "Choose", value: "" },
+                                        { text: "Aisle", value: "aisle" },
+                                        { text: "Middle", value: "middle" },
+                                        { text: "Window", value: "window" }
+                                    ]}
+                                />
+                            </div>
+                            <div className="flex flex-col">
+                                <Label text="Aircraft Side" />
+                                <Select
+                                    name="aircraftSide"
+                                    options={[
+                                        { text: "Choose", value: "" },
+                                        { text: "Left", value: "left" },
+                                        { text: "Right", value: "right" },
+                                        { text: "Center", value: "center" }
+                                    ]}
+                                />
+                            </div>
+                            <div className="flex flex-col">
+                                <Label text="Class" />
+                                <Select
+                                    name="ticketClass"
+                                    options={[
+                                        { text: "Choose", value: "" },
+                                        { text: "Private", value: "private" },
+                                        { text: "First", value: "first" },
+                                        { text: "Business", value: "business" },
+                                        { text: "Economy+", value: "economy+" },
+                                        { text: "Economy", value: "economy" }
+                                    ]}
+                                />
+                            </div>
+                            <div className="flex flex-col">
+                                <Label text="Purpose" />
+                                <Select
+                                    name="purpose"
+                                    options={[
+                                        { text: "Choose", value: "" },
+                                        { text: "Leisure", value: "leisure" },
+                                        { text: "Business", value: "business" },
+                                        { text: "Crew", value: "crew" },
+                                        { text: "Other", value: "other" }
+                                    ]}
+                                />
+                            </div>
+                        </div>
+
+                        <br />
+                        <div className="flex flex-wrap gap-4">
+                            <div className="flex flex-col">
+                                <Label text="Airplane" />
+                                <Input
+                                    type="text"
+                                    name="airplane"
+                                    placeholder="B738"
+                                    maxLength={16}
+                                />
+                            </div>
+                            <div className="flex flex-col">
+                                <Label text="Flight Number" />
+                                <Input
+                                    type="text"
+                                    name="flightNumber"
+                                    placeholder="FR2460"
+                                    maxLength={7}
+                                />
+                            </div>
+                        </div>
+                        <br />
+                        <Label text="Notes" />
+                        <TextArea
+                            name="notes"
+                            maxLength={150}
+                        />
+                    </div>
                 </div>
 
-                <div className="container">
-                    <Label text="Departure Time" />
-                    <Input type="time"
-                           name="departureTime" />
-                    <br />
-                    <Label text="Arrival Time"/>
-                    <Input type="time"
-                           name="arrivalTime"/>
-                    <br />
-                    <Label text="Arrival Date" />
-                    <Input type="date"
-                           name="arrivalDate" />
-                </div>
-
-                <div className="container">
-                    <Label text="Seat Type"/>
-                    <Select name="seat"
-                            options={[
-                                { text: "Choose", value: "" },
-                                { text: "Aisle", value: "aisle" },
-                                { text: "Middle", value: "middle" },
-                                { text: "Window", value: "window" }
-                            ]} />
-                    <br />
-                    <Label text="Aircraft Side"/>
-                    <Select name="aircraftSide"
-                            options={[
-                                { text: "Choose", value: "" },
-                                { text: "Left", value: "left" },
-                                { text: "Right", value: "right" },
-                                { text: "Center", value: "center" }
-                            ]} />
-                    <br />
-                    <Label text="Class"/>
-                    <Select name="ticketClass"
-                            options={[
-                                { text: "Choose", value: "" },
-                                { text: "Private", value: "private" },
-                                { text: "First", value: "first" },
-                                { text: "Business", value: "business" },
-                                { text: "Economy+", value: "economy+" },
-                                { text: "Economy", value: "economy" }
-                            ]} />
-                    <br />
-                    <Label text="Purpose"/>
-                    <Select name="purpose"
-                            options={[
-                                { text: "Choose", value: "" },
-                                { text: "Leisure", value: "leisure" },
-                                { text: "Business", value: "business" },
-                                { text: "Crew", value: "crew" },
-                                { text: "Other", value: "other" }
-                            ]} />
-                    <br />
-                    <Label text="Airplane"/>
-                    <Input type="text"
-                           name="airplane"
-                           placeholder="B738"
-                           maxLength={16} />
-                    <br />
-                    <Label text="Flight Number"/>
-                    <Input type="text"
-                           name="flightNumber"
-                           placeholder="FR2460"
-                           maxLength={7} />
-                    <br />
-                    <Label text="Notes"/>
-                    <TextArea name="notes"
-                              maxLength={150} />
-                </div>
-
-            </div>
-
-            <Button text="Done"
+                <Button
+                    text="Done"
                     level="success"
-                    submit />
-        </form>
-    </>
+                    submit
+                />
+            </form>
+        </>
     );
 }
