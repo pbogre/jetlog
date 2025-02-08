fd928a9bb06f70c4b884237af4156dad0a928d6a
Pietro Bonaldo <pietro.bogre@gmail.com>
2025-02-08 17:23:26 +0100

    Import flight purpose from MyFlightRadar24 CSV

--- server/routers/importing.py
+++ server/routers/importing.py
@@ -1,4 +1,4 @@
-from server.models import FlightModel, SeatType, ClassType, User
+from server.models import FlightModel, FlightPurpose, SeatType, ClassType, User
 from server.routers.flights import add_flight
 from server.auth.users import get_current_user
 
@@ -71,6 +71,8 @@ async def import_CSV(csv_type: CSVType,
                 # from myflightradar24, 0=none, 1=window, 2=middle, 3=aisle
                 values_dict['seat'] = list(SeatType)[int(values[11]) - 1] if int(values[11]) > 0 else None
                 values_dict['ticket_class'] = ticket_class_conversion[values[12]] if int(values[12]) > 0 else None
+                # from myflightradar24, 0=none, 1=leisure, 2=business, 3=crew, 4=other
+                values_dict['purpose'] = list(FlightPurpose)[int(values[13]) - 1] if int(values[13]) > 0 else None
                 # conversion from hh:mm:ss to minutes
                 values_dict['duration'] = int(values[6][:2]) * 60 + int(values[6][3:5]) 
                 values_dict['airplane'] = values[8].replace('"', '') if values[8] != '" ()"' else None
