a57ce62e7617b0287af79462746c2ed1eed8068b
Pietro Bonaldo <pietro.bogre@gmail.com>
2025-03-24 19:11:32 +0100

    (hotfix) Make AUTH_HEADER optional

--- server/environment.py
+++ server/environment.py
@@ -1,23 +1,25 @@
 import os
 import sys
 
-def _get_environment_variable(key: str, cast_int: bool = False) -> str|int:
+def _get_environment_variable(key: str, cast_int: bool = False, required: bool = True) -> str|int|None:
     value = os.environ.get(key)
 
     if not value:
-        # env variable is necessary
-        print(f"Environment variable '{key}' is not set. Aborting...")
-        sys.exit(1)
+        if required:
+            print(f"Environment variable '{key}' is not set. Aborting...")
+            sys.exit(1)
+        return None
 
     if cast_int:
         try:
             return int(value)
-        except:
-            print("Environment variable '{key}' should be an integer, got '{value}'")
+        except ValueError:
+            print(f"Environment variable '{key}' should be an integer, got '{value}'")
+            sys.exit(1)
 
     return value
 
 DATA_PATH = _get_environment_variable("DATA_PATH")
 SECRET_KEY = _get_environment_variable("SECRET_KEY")
-AUTH_HEADER = _get_environment_variable("AUTH_HEADER")
-TOKEN_DURATION = int(_get_environment_variable("TOKEN_DURATION", cast_int=True)) # double cast to make linter happy
+AUTH_HEADER = _get_environment_variable("AUTH_HEADER", required=False)
+TOKEN_DURATION = _get_environment_variable("TOKEN_DURATION", cast_int=True)
